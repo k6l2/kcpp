781a8d345d581beab35b017874ce6b73b1942d61
Kyle <ssjmetroid@gmail.com>
2020-05-19 13:54:04 -0700

    Preserve environment vars in build script.

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
+set project_root_COPY=%project_root%
+set exe_name_COPY=%exe_name%
 set project_root=%~dp0
 set exe_name=kc++
 pushd %project_root%
@@ -11,6 +13,7 @@ pushd %project_root%\code
 FOR /F "delims=" %%G IN ('DIR /B /S') DO (
 	>>"%project_root%\build\code-tree-current.txt" ECHO %%~G,%%~tG,%%~zG
 )
+rem pop from %project_root%\code
 popd
 pushd build
 rem --- Detect if the code tree differs, and if it doesn't, skip building ---
@@ -31,5 +34,9 @@ rem --- Build the executable ---
 cl %project_root%\code\main.cpp /Fe%exe_name% /nologo /std:c++latest ^
 	/Od /Oi /GR- /EHsc /Zi /FC /link /incremental:no 
 :SKIP_BUILD
+rem pop from build
 popd
+rem pop from %project_root%
 popd
+set project_root=%project_root_COPY%
+set exe_name=%exe_name_COPY%
