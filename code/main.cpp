ac78978eb3f37a8ae9c7215ffac2f3ca10272f3d
Kyle <ssjmetroid@gmail.com>
2020-11-06 14:05:23 -0800

    generate empty polymorphic tagged union
    - pull tokenizer code out of main

--- code/main.cpp
+++ code/main.cpp
@@ -9,266 +9,16 @@ using std::vector;
 using std::string;
 #include <sstream>
 using std::stringstream;
+#include <set>
+using std::set;
 namespace chrono = std::chrono;
 namespace fs = std::filesystem;
 static bool g_verbose;
+#if KASSET_IMPLEMENTATION
 static vector<string> g_kassets;
-enum class KTokenType : int8_t
-{
-	PAREN_OPEN,
-	PAREN_CLOSE,
-	COLON,
-	SEMICOLON,
-	ASTERISK,
-	BRACKET_OPEN,
-	BRACKET_CLOSE,
-	BRACE_OPEN,
-	BRACE_CLOSE,
-	HASH_TAG,
-	WHITESPACE,
-	COMMENT,
-	STRING,
-	CHARACTER,
-	IDENTIFIER,
-	END_OF_STREAM,
-	UNKNOWN
-};
-struct KToken
-{
-	KTokenType type;
-	int textLength;
-	const char* text;
-};
-struct KTokenizer
-{
-	const char* at;
-};
-static bool isEndOfLine(char c)
-{
-	const bool result = 
-		c == '\r' ||
-		c == '\n';
-	return result;
-}
-static bool isWhitespace(char c)
-{
-	const bool result = 
-		c == ' ' ||
-		c == '\t' ||
-		isEndOfLine(c);
-	return result;
-}
-static KToken ktokeParseWhitespace(KTokenizer& tokenizer)
-{
-	KToken result = {
-		.type = KTokenType::WHITESPACE,
-		.textLength = 0,
-		.text = tokenizer.at
-	};
-	while(isWhitespace(tokenizer.at[0]))
-	{
-		tokenizer.at++;
-		result.textLength++;
-	}
-	return result;
-}
-static KToken ktokeParseComment(KTokenizer& tokenizer)
-{
-	KToken result = {
-		.type = KTokenType::COMMENT,
-		.textLength = 0,
-		.text = tokenizer.at
-	};
-	if(tokenizer.at[0] == '/' && tokenizer.at[1] == '/')
-	{
-		tokenizer.at += 2;
-		result.textLength += 2;
-		while(tokenizer.at[0] && !isEndOfLine(tokenizer.at[0]))
-		{
-			tokenizer.at++;
-			result.textLength++;
-		}
-		return result;
-	}
-	else if(tokenizer.at[0] == '/' && tokenizer.at[1] == '*')
-	{
-		tokenizer.at += 2;
-		result.textLength += 2;
-		while(tokenizer.at[0] && 
-			!(tokenizer.at[0] == '*' && tokenizer.at[1] == '/'))
-		{
-			tokenizer.at++;
-			result.textLength++;
-		}
-		if(tokenizer.at[0] == '*')
-		{
-			tokenizer.at += 2;
-			result.textLength += 2;
-		}
-		return result;
-	}
-	return result;
-}
-static bool isAlpha(char c)
-{
-	const bool result = 
-		(c >= 'a' && c <= 'z') ||
-		(c >= 'A' && c <= 'Z') ||
-		c == '_';
-	return result;
-}
-static bool isNumeric(char c)
-{
-	const bool result = (c >= '0' && c <= '9');
-	return result;
-}
-KToken ktokeParseIdentifier(KTokenizer& tokenizer)
-{
-	KToken result = {
-		.type = KTokenType::IDENTIFIER,
-		.textLength = 1,
-		.text = tokenizer.at
-	};
-	tokenizer.at++;
-	while(isAlpha(tokenizer.at[0]) || isNumeric(tokenizer.at[0]))
-	{
-		tokenizer.at++;
-		result.textLength++;
-	}
-	return result;
-}
-#if 0
-KToken ktokeParseNumber(KTokenizer& tokenizer)
-{
-}
-#endif // 0
-static KToken ktokeNext(KTokenizer& tokenizer)
-{
-	if(isWhitespace(tokenizer.at[0]))
-	{
-		return ktokeParseWhitespace(tokenizer);
-	}
-	KToken result = {
-		.type = KTokenType::UNKNOWN,
-		.textLength = 1,
-		.text = tokenizer.at
-	};
-	switch(tokenizer.at[0])
-	{
-		case '\0': result.type = KTokenType::END_OF_STREAM; tokenizer.at++; break;
-		case '(':  result.type = KTokenType::PAREN_OPEN;    tokenizer.at++; break;
-		case ')':  result.type = KTokenType::PAREN_CLOSE;   tokenizer.at++; break;
-		case ':':  result.type = KTokenType::COLON;         tokenizer.at++; break;
-		case ';':  result.type = KTokenType::SEMICOLON;     tokenizer.at++; break;
-		case '*':  result.type = KTokenType::ASTERISK;      tokenizer.at++; break;
-		case '[':  result.type = KTokenType::BRACKET_OPEN;  tokenizer.at++; break;
-		case ']':  result.type = KTokenType::BRACKET_CLOSE; tokenizer.at++; break;
-		case '{':  result.type = KTokenType::BRACE_OPEN;    tokenizer.at++; break;
-		case '}':  result.type = KTokenType::BRACE_CLOSE;   tokenizer.at++; break;
-		case '#':  result.type = KTokenType::HASH_TAG;      tokenizer.at++; break;
-		case '/':
-		{
-			if(tokenizer.at[1] && 
-				(tokenizer.at[1] == '/' || tokenizer.at[1] == '*'))
-			{
-				return ktokeParseComment(tokenizer);
-			}
-			// otherwise, this is something else like a division operator. Skip.
-			tokenizer.at++;
-		}break;
-		case '"': 
-		{
-			result.type = KTokenType::STRING;
-			result.textLength = 0;
-			tokenizer.at++;
-			result.text = tokenizer.at;
-			while(tokenizer.at[0] && tokenizer.at[0] != '"')
-			{
-				if(tokenizer.at[0] == '\\' && tokenizer.at[1])
-				{
-					tokenizer.at++; result.textLength++;
-				}
-				tokenizer.at++; result.textLength++;
-			}
-			// consume the final '"' 
-			if(tokenizer.at[0] == '"')
-			{
-				tokenizer.at++;
-			}
-		}break;
-		case '\'':
-		{
-			result.type = KTokenType::CHARACTER;
-			result.textLength = 0;
-			tokenizer.at++;
-			result.text = tokenizer.at;
-			while(tokenizer.at[0] && tokenizer.at[0] != '\'')
-			{
-				if(tokenizer.at[0] == '\\' && tokenizer.at[1])
-				{
-					tokenizer.at++; result.textLength++;
-				}
-				tokenizer.at++; result.textLength++;
-			}
-			// consume the final '\''
-			if(tokenizer.at[0] == '\'')
-			{
-				tokenizer.at++;
-			}
-		}break;
-		default:
-		{
-			if(isAlpha(tokenizer.at[0]))
-			{
-				return ktokeParseIdentifier(tokenizer);
-			}
-#if 0
-			else if(isNumeric(tokenizer.at[0]))
-			{
-				return ktokeParseNumber(tokenizer);
-			}
-#endif// 0
-			else 
-			{
-				// skip our UNKNOWN token character
-				tokenizer.at++;
-			}
-		}break;
-	}
-	return result;
-}
-static bool ktokeEquals(const KToken& token, const char* cStr)
-{
-	const char* cStrCurr = cStr;
-	for(int c = 0; c < token.textLength; c++, cStrCurr++)
-	{
-		if(cStr[c] == '\0' || token.text[c] != cStr[c])
-		{
-			return false;
-		}
-	}
-	const bool result = *cStrCurr == '\0';
-	return result;
-}
-static KToken kcppRequireToken(KTokenizer& tokenizer, KTokenType tokenType)
-{
-	KToken token;
-	do
-	{
-		token = ktokeNext(tokenizer);
-#if 0
-		if(token.type != tokenType &&
-			(token.type == KTokenType::END_OF_STREAM ||
-				!(token.type == KTokenType::WHITESPACE ||
-				token.type == KTokenType::COMMENT)))
-#endif// 0
-		if(token.type == KTokenType::END_OF_STREAM)
-		{
-			return token;
-		}
-	} while(token.type != tokenType);
-	return token;
-}
+#endif// KASSET_IMPLEMENTATION
+static set<string> g_polyTaggedUnions;
+#include "tokenizer.cpp"
 #define PARSE_FAILURE() \
 	{ fprintf(stderr, "parse failure!\n");\
 	  assert(false);\
@@ -306,7 +56,7 @@ static void
 			PARSE_FAILURE();
 		const string tokenStr = 
 			string(tokenStructId.text, tokenStructId.textLength);
-		printf("KGT_POLYMORPHIC_TAGGED_UNION: '%s'\n", tokenStr.c_str());
+		g_polyTaggedUnions.insert(tokenStr);
 	}
 }
 #if KASSET_IMPLEMENTATION
@@ -876,6 +626,16 @@ vector<fs::path>
 	}
 	return result;
 }
+static string 
+	generatePolymorphicTaggedUnion(const string& ptuIdentifier)
+{
+	string result;
+	result.append("union\n");
+	result.append("{\n");
+	result.append("	void* no_derived_structs;\n");
+	result.append("};\n");
+	return result;
+}
 int 
 	main(int argc, char** argv)
 {
@@ -913,6 +673,8 @@ int
 		}
 		printf("output='%ws'\n", fsPathOutput.c_str());
 	}
+	/* parse all the files recursively in all the provided input directories */
+	/* @TODO: ignore files that aren't C++ */
 	int result = EXIT_SUCCESS;
 	for(const fs::path& fsPathInput : vecFsPathInputs)
 	{
@@ -939,6 +701,20 @@ int
 				        fsDirEnt.path().c_str());
 				result = EXIT_FAILURE;
 			}
+		}
+	}
+	/* output generated code into the provided output directory */
+	fs::create_directories(fsPathOutput);
+	for(const string& ptuIdentifier : g_polyTaggedUnions)
+	{
+		const string ptuGenFileName = "gen_" + ptuIdentifier + ".h";
+		const fs::path outPath = fsPathOutput / ptuGenFileName;
+		const string fileData = generatePolymorphicTaggedUnion(ptuIdentifier);
+		if(!writeEntireFile(outPath.c_str(), fileData.c_str()))
+		{
+			fprintf(stderr, "Failed to write file '%ws'!\n", 
+					outPath.c_str());
+			result = EXIT_FAILURE;
 		}
 	}
 #if 0
---
+++ code/tokenizer.cpp
@@ -0,0 +1,257 @@
+#include <cstdint>
+enum class KTokenType : int8_t
+{
+	PAREN_OPEN,
+	PAREN_CLOSE,
+	COLON,
+	SEMICOLON,
+	ASTERISK,
+	BRACKET_OPEN,
+	BRACKET_CLOSE,
+	BRACE_OPEN,
+	BRACE_CLOSE,
+	HASH_TAG,
+	WHITESPACE,
+	COMMENT,
+	STRING,
+	CHARACTER,
+	IDENTIFIER,
+	END_OF_STREAM,
+	UNKNOWN
+};
+struct KToken
+{
+	KTokenType type;
+	int textLength;
+	const char* text;
+};
+struct KTokenizer
+{
+	const char* at;
+};
+static bool isEndOfLine(char c)
+{
+	const bool result = 
+		c == '\r' ||
+		c == '\n';
+	return result;
+}
+static bool isWhitespace(char c)
+{
+	const bool result = 
+		c == ' ' ||
+		c == '\t' ||
+		isEndOfLine(c);
+	return result;
+}
+static KToken ktokeParseWhitespace(KTokenizer& tokenizer)
+{
+	KToken result = {
+		.type = KTokenType::WHITESPACE,
+		.textLength = 0,
+		.text = tokenizer.at
+	};
+	while(isWhitespace(tokenizer.at[0]))
+	{
+		tokenizer.at++;
+		result.textLength++;
+	}
+	return result;
+}
+static KToken ktokeParseComment(KTokenizer& tokenizer)
+{
+	KToken result = {
+		.type = KTokenType::COMMENT,
+		.textLength = 0,
+		.text = tokenizer.at
+	};
+	if(tokenizer.at[0] == '/' && tokenizer.at[1] == '/')
+	{
+		tokenizer.at += 2;
+		result.textLength += 2;
+		while(tokenizer.at[0] && !isEndOfLine(tokenizer.at[0]))
+		{
+			tokenizer.at++;
+			result.textLength++;
+		}
+		return result;
+	}
+	else if(tokenizer.at[0] == '/' && tokenizer.at[1] == '*')
+	{
+		tokenizer.at += 2;
+		result.textLength += 2;
+		while(tokenizer.at[0] && 
+			!(tokenizer.at[0] == '*' && tokenizer.at[1] == '/'))
+		{
+			tokenizer.at++;
+			result.textLength++;
+		}
+		if(tokenizer.at[0] == '*')
+		{
+			tokenizer.at += 2;
+			result.textLength += 2;
+		}
+		return result;
+	}
+	return result;
+}
+static bool isAlpha(char c)
+{
+	const bool result = 
+		(c >= 'a' && c <= 'z') ||
+		(c >= 'A' && c <= 'Z') ||
+		c == '_';
+	return result;
+}
+static bool isNumeric(char c)
+{
+	const bool result = (c >= '0' && c <= '9');
+	return result;
+}
+KToken ktokeParseIdentifier(KTokenizer& tokenizer)
+{
+	KToken result = {
+		.type = KTokenType::IDENTIFIER,
+		.textLength = 1,
+		.text = tokenizer.at
+	};
+	tokenizer.at++;
+	while(isAlpha(tokenizer.at[0]) || isNumeric(tokenizer.at[0]))
+	{
+		tokenizer.at++;
+		result.textLength++;
+	}
+	return result;
+}
+#if 0
+KToken ktokeParseNumber(KTokenizer& tokenizer)
+{
+}
+#endif // 0
+static KToken ktokeNext(KTokenizer& tokenizer)
+{
+	if(isWhitespace(tokenizer.at[0]))
+	{
+		return ktokeParseWhitespace(tokenizer);
+	}
+	KToken result = {
+		.type = KTokenType::UNKNOWN,
+		.textLength = 1,
+		.text = tokenizer.at
+	};
+	switch(tokenizer.at[0])
+	{
+		case '\0': result.type = KTokenType::END_OF_STREAM; tokenizer.at++; break;
+		case '(':  result.type = KTokenType::PAREN_OPEN;    tokenizer.at++; break;
+		case ')':  result.type = KTokenType::PAREN_CLOSE;   tokenizer.at++; break;
+		case ':':  result.type = KTokenType::COLON;         tokenizer.at++; break;
+		case ';':  result.type = KTokenType::SEMICOLON;     tokenizer.at++; break;
+		case '*':  result.type = KTokenType::ASTERISK;      tokenizer.at++; break;
+		case '[':  result.type = KTokenType::BRACKET_OPEN;  tokenizer.at++; break;
+		case ']':  result.type = KTokenType::BRACKET_CLOSE; tokenizer.at++; break;
+		case '{':  result.type = KTokenType::BRACE_OPEN;    tokenizer.at++; break;
+		case '}':  result.type = KTokenType::BRACE_CLOSE;   tokenizer.at++; break;
+		case '#':  result.type = KTokenType::HASH_TAG;      tokenizer.at++; break;
+		case '/':
+		{
+			if(tokenizer.at[1] && 
+				(tokenizer.at[1] == '/' || tokenizer.at[1] == '*'))
+			{
+				return ktokeParseComment(tokenizer);
+			}
+			// otherwise, this is something else like a division operator. Skip.
+			tokenizer.at++;
+		}break;
+		case '"': 
+		{
+			result.type = KTokenType::STRING;
+			result.textLength = 0;
+			tokenizer.at++;
+			result.text = tokenizer.at;
+			while(tokenizer.at[0] && tokenizer.at[0] != '"')
+			{
+				if(tokenizer.at[0] == '\\' && tokenizer.at[1])
+				{
+					tokenizer.at++; result.textLength++;
+				}
+				tokenizer.at++; result.textLength++;
+			}
+			// consume the final '"' 
+			if(tokenizer.at[0] == '"')
+			{
+				tokenizer.at++;
+			}
+		}break;
+		case '\'':
+		{
+			result.type = KTokenType::CHARACTER;
+			result.textLength = 0;
+			tokenizer.at++;
+			result.text = tokenizer.at;
+			while(tokenizer.at[0] && tokenizer.at[0] != '\'')
+			{
+				if(tokenizer.at[0] == '\\' && tokenizer.at[1])
+				{
+					tokenizer.at++; result.textLength++;
+				}
+				tokenizer.at++; result.textLength++;
+			}
+			// consume the final '\''
+			if(tokenizer.at[0] == '\'')
+			{
+				tokenizer.at++;
+			}
+		}break;
+		default:
+		{
+			if(isAlpha(tokenizer.at[0]))
+			{
+				return ktokeParseIdentifier(tokenizer);
+			}
+#if 0
+			else if(isNumeric(tokenizer.at[0]))
+			{
+				return ktokeParseNumber(tokenizer);
+			}
+#endif// 0
+			else 
+			{
+				// skip our UNKNOWN token character
+				tokenizer.at++;
+			}
+		}break;
+	}
+	return result;
+}
+static bool ktokeEquals(const KToken& token, const char* cStr)
+{
+	const char* cStrCurr = cStr;
+	for(int c = 0; c < token.textLength; c++, cStrCurr++)
+	{
+		if(cStr[c] == '\0' || token.text[c] != cStr[c])
+		{
+			return false;
+		}
+	}
+	const bool result = *cStrCurr == '\0';
+	return result;
+}
+static KToken kcppRequireToken(KTokenizer& tokenizer, KTokenType tokenType)
+{
+	KToken token;
+	do
+	{
+		token = ktokeNext(tokenizer);
+#if 0
+		if(token.type != tokenType &&
+			(token.type == KTokenType::END_OF_STREAM ||
+				!(token.type == KTokenType::WHITESPACE ||
+				token.type == KTokenType::COMMENT)))
+#endif// 0
+		if(token.type == KTokenType::END_OF_STREAM)
+		{
+			return token;
+		}
+	} while(token.type != tokenType);
+	return token;
+}
